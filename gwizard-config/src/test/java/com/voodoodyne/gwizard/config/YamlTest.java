e9faee0ef382c0e8239606f474a82799f9600fd1
Jeff Schnitzer <jeff@infohazard.org>
2015-01-23 09:57:49 -0800

    Simple test of yaml parser behavior

---
+++ gwizard-config/src/test/java/com/voodoodyne/gwizard/config/YamlTest.java
@@ -0,0 +1,34 @@
+package com.voodoodyne.gwizard.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import java.io.InputStream;
+import java.util.Map;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.nullValue;
+
+public class YamlTest {
+
+	//@Test
+	public void readYamlNull() throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		YAMLFactory yamlFactory = new YAMLFactory();
+
+		InputStream input = getClass().getResourceAsStream("/has_null.yml");
+
+		Map<String, Object> map = mapper.readValue(yamlFactory.createParser(input), Map.class);
+
+		// Literal 'null' seems to produce the string "null"
+		assert map.containsKey("first");
+		assertThat(map.get("first"), nullValue());
+
+		// Literal '~' seems to produce the string "~"
+		assert map.containsKey("second");
+		assertThat(map.get("second"), nullValue());
+
+		// Literal '!!null' seems to produce an empty string
+		assert map.containsKey("third");
+		assertThat(map.get("third"), nullValue());
+	}
+
+}
