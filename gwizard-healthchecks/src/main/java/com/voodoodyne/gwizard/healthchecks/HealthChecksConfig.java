771021b5c4bb284bbad57446226aabe35e49ad8e
Jeff Schnitzer <jeff@infohazard.org>
2015-01-23 11:37:21 -0800

    Disable periodic health checks by default

--- gwizard-healthchecks/src/main/java/com/voodoodyne/gwizard/healthchecks/HealthChecksConfig.java
+++ gwizard-healthchecks/src/main/java/com/voodoodyne/gwizard/healthchecks/HealthChecksConfig.java
@@ -6,8 +6,10 @@ import lombok.Data;
 @Data
 public class HealthChecksConfig {
 	/**
-	 * Run health checks periodically at this interval. Failing health checks will be logged at level WARN.
-	 * This accepts a flexible format like "10 minutes" or "30s"
+	 * <p>Run health checks periodically at this interval. Failing health checks will be logged at level WARN.
+	 * This accepts a flexible format like "10 minutes" or "30s".</p>
+	 *
+	 * Default is null, which disables periodic health checks.
 	 */
-	private Duration interval = Duration.minutes(10);
+	private Duration interval = null; //Duration.minutes(10);
 }
