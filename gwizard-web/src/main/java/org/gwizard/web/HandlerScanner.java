7d83b701ff345ff8c95c9713b46a4648d86bd2b3
Scott Bessler <mbessler@mbessler-ltm.internal.salesforce.com>
2015-05-29 10:31:26 -0700

    add HandlerScanner to allow adding jetty handlers through injection

---
+++ gwizard-web/src/main/java/org/gwizard/web/HandlerScanner.java
@@ -0,0 +1,46 @@
+package org.gwizard.web;
+
+import com.google.inject.Binding;
+import com.google.inject.Injector;
+import org.eclipse.jetty.server.Handler;
+
+import javax.inject.Inject;
+import java.lang.reflect.Type;
+import java.util.EventListener;
+
+/**
+ * Walks through the guice injector bindings, visiting each one that is a Handler.
+ */
+public class HandlerScanner {
+	public interface Visitor {
+		void visit(Handler handler);
+	}
+
+	private final Injector injector;
+
+	@Inject
+	public HandlerScanner(Injector injector) {
+		this.injector = injector;
+	}
+
+	/** Start the process, visiting each ServletContextListener bound in the injector or any parents */
+	public void accept(Visitor visitor) {
+		accept(injector, visitor);
+	}
+
+	/** Recursive impl that walks up the parent injectors first */
+	private void accept(Injector inj, Visitor visitor) {
+		if (inj == null)
+			return;
+
+		accept(inj.getParent(), visitor);
+
+		for (final Binding<?> binding: inj.getBindings().values()) {
+			final Type type = binding.getKey().getTypeLiteral().getType();
+
+			if (type instanceof Class && Handler.class.isAssignableFrom((Class)type)) {
+				visitor.visit((Handler)binding.getProvider().get());
+			}
+		}
+	}
+}
