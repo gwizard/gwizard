bd2443cf3bb75a4419904cd5ef3886b666ec63b3
Jeff Schnitzer <jeff@infohazard.org>
2024-11-12 07:51:58 -0800

    Bump to trivet v2.3

--- gwizard-rpc/src/main/java/org/gwizard/rpc/RpcServlet.java
+++ gwizard-rpc/src/main/java/org/gwizard/rpc/RpcServlet.java
@@ -2,9 +2,10 @@ package org.gwizard.rpc;
 
 import com.google.inject.Injector;
 import com.voodoodyne.trivet.TrivetServlet;
-
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+
+import java.io.Serial;
 
 /**
  * The glue that turns an RPC request into a method call
@@ -12,15 +13,12 @@ import jakarta.inject.Singleton;
 @Singleton
 public class RpcServlet extends TrivetServlet {
 
-	private final Injector injector;
+	@Serial
+	private static final long serialVersionUID = 1L;
 
 	@Inject
-	public RpcServlet(Injector injector) {
-		this.injector = injector;
+	public RpcServlet(final Injector injector) {
+		super(injector::getInstance);
 	}
 
-	@Override
-	public Object getInstance(Class<?> aClass) {
-		return injector.getInstance(aClass);
-	}
 }
